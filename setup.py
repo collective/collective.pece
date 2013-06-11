4500e8078e9f90a1b23cbc80eb5177daca35adde
Alex Clark <aclark@aclark.net>
2013-06-11 16:39:07 -0400

    Fix syntax

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 
 setup(
     name='rpi.asthma_files_site',
-    package=find_packages(),
-    namespace_package=['rpi'],
+    packages=find_packages(),
+    namespace_packages=['rpi'],
     entry_points={
         'z3c.autoinclude.plugin': 'target = plone',
     }
