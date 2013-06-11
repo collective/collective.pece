61137adf7a804d552a7c47453c9e6a9286a8d590
Alex Clark <aclark@aclark.net>
2013-06-11 16:34:24 -0400

    Configure package

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+from setuptools import find_packages
 from setuptools import setup
 
 
 setup(
     name='rpi.asthma_files_site',
+    package=find_packages(),
+    namespace_package=['rpi'],
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = plone',
+    }
 )
