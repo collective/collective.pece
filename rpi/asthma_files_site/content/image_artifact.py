7372934a8439a31bb74e2d5287be1417c15da6e2
Alex Clark <aclark@aclark.net>
2013-06-27 19:34:13 -0400

    Make it work

--- rpi/asthma_files_site/content/image_artifact.py
+++ rpi/asthma_files_site/content/image_artifact.py
@@ -11,5 +11,9 @@ class ImageArtifactView():
     def __call__(self):
         return self.image_artifact_view()
 
+    def get_image(self):
+        return '/'.join([self.context.absolute_url(),
+            self.context.image.filename], 'image_preview')
+
     def get_questions(self):
         return self.context.portal_catalog(portal_type="question")
