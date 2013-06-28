68c5cd968400684665b775ded599d810e96c7a35
Alex Clark <aclark@aclark.net>
2013-06-27 21:09:00 -0400

    Make it work

--- rpi/asthma_files_site/content/image_artifact.py
+++ rpi/asthma_files_site/content/image_artifact.py
@@ -1,4 +1,5 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+import base64
 
 
 class ImageArtifactView():
@@ -12,8 +13,8 @@ class ImageArtifactView():
         return self.image_artifact_view()
 
     def get_image(self):
-        return "data:%s;%s" % (self.context.image.contentType,
-            self.context.image.data)
+        return "data:%s;base64,%s" % (self.context.image.contentType,
+            base64.encodestring(self.context.image.data))
 
     def get_questions(self):
         return self.context.portal_catalog(portal_type="question")
