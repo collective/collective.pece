42a20539a283b8fe484550e0154f9d1d348e7670
Alex Clark <aclark@aclark.net>
2013-06-29 09:33:32 -0400

    Clean up

--- rpi/asthma_files_site/content/image_artifact.py
+++ rpi/asthma_files_site/content/image_artifact.py
@@ -1,5 +1,4 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-import base64
 
 
 class ImageArtifactView():
@@ -10,10 +9,6 @@ class ImageArtifactView():
 
     def __call__(self):
         return self.image_artifact_view()
-
-    def get_image(self):
-        return "data:%s;base64,%s" % (self.context.image.contentType,
-            base64.encodestring(self.context.image.data))
 
     def get_questions(self):
         return self.context.portal_catalog(portal_type="question")
