bae722b2971c894952dc3bdd7d120dc104acc4fd
Alex Clark <aclark@aclark.net>
2013-06-30 09:02:28 -0400

    Fix sorting

--- rpi/asthma_files_site/content/image_artifact.py
+++ rpi/asthma_files_site/content/image_artifact.py
@@ -12,4 +12,5 @@ class ImageArtifactView():
 
     def get_questions(self):
         return self.context.portal_catalog(portal_type="question",
-            sort_order="reverse")
+            sort_on="id",
+            sort_order="ascending")
