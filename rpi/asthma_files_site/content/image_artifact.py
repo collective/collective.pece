8f37c5f2cfcd7a20203eaa8d8bd910045ead34ac
Alex Clark <aclark@aclark.net>
2013-07-18 17:59:18 -0400

    Flake8

--- rpi/asthma_files_site/content/image_artifact.py
+++ rpi/asthma_files_site/content/image_artifact.py
@@ -14,15 +14,25 @@ class IImageArtifact(form.Schema):
     """
 
     image = NamedBlobImage(title=u"Image")
+
     coverage = TextLine(title=u"Coverage", required=False)
+
     dublin_core_format = TextLine(title=u"Format", required=False)
+
     identifier = TextLine(title=u"Identifier", required=False)
+
     language = TextLine(title=u"Language", required=False)
+
     publisher = TextLine(title=u"Publisher", required=False)
+
     relation = TextLine(title=u"Relation", required=False)
+
     rights = TextLine(title=u"Rights", required=False)
+
     source = TextLine(title=u"Source", required=False)
+
     subject = TextLine(title=u"Subject", required=False)
+
     dublin_core_type = TextLine(title=u"Type", required=False)
 
 
