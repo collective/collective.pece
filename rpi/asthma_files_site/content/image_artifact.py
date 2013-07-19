bb55702b8c4208ea3881f169d6e0e1af37ab41bc
Alex Clark <aclark@aclark.net>
2013-07-19 09:19:53 -0400

    Factor out artifact_view

--- rpi/asthma_files_site/content/image_artifact.py
+++ rpi/asthma_files_site/content/image_artifact.py
@@ -1,22 +1,18 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.uuid.utils import uuidToObject
-from plone.dexterity.utils import createContentInContainer
 from plone.directives import form
 from plone.namedfile.field import NamedBlobImage
-from zope.annotation.interfaces import IAnnotations as IStorage  # Avoid
-                                # confusion with annotation content type
 from zope.schema import TextLine
+
 # XXX Poor man's Dublin Core (re)implementation
-from .dublin_core import _coverage
-from .dublin_core import _format
-from .dublin_core import _identifier
-from .dublin_core import _language
-from .dublin_core import _publisher
-from .dublin_core import _relation
-from .dublin_core import _rights
-from .dublin_core import _source
-from .dublin_core import _subject
-from .dublin_core import _type
+#from .dublin_core import _coverage
+#from .dublin_core import _format
+#from .dublin_core import _identifier
+#from .dublin_core import _language
+#from .dublin_core import _publisher
+#from .dublin_core import _relation
+#from .dublin_core import _rights
+#from .dublin_core import _source
+#from .dublin_core import _subject
+#from .dublin_core import _type
 
 
 class IImageArtifact(form.Schema):
@@ -24,138 +20,41 @@ class IImageArtifact(form.Schema):
     Content type
     """
 
-    image = NamedBlobImage(title=u"Image")
+    image_artifact = NamedBlobImage(title=u"Image Artifact")
 
     # XXX Poor man's Dublin Core (re)implementation
-    coverage = _coverage
-    dublin_core_format = _format
-    identifier = _identifier
-    language = _language
-    publisher = _publisher
-    relation = _relation
-    rights = _rights
-    source = _source
-    subject = _subject
-    dublin_core_type = _type
+#    coverage = _coverage
+#    dublin_core_format = _format
+#    identifier = _identifier
+#    language = _language
+#    publisher = _publisher
+#    relation = _relation
+#    rights = _rights
+#    source = _source
+#    subject = _subject
+#    dublin_core_type = _type
 
 
-class ImageArtifactEditMetadata():
-    """
-    Browser page. XXX Avoids (or postpones) having to customize the edit form,
-    which we may need to do anyway.
-    """
-
-    def __call__(self):
-        """
-        Set metadata; redirect to context
-        """
-        contributor = self.request.form.get('contributor').split('\r\n')
-        self.context.contributors = contributor
-
-        coverage = self.request.form.get('coverage')
-        self.context.coverage = coverage
-
-        creator = self.request.form.get('creator').split('\r\n')
-        self.context.creators = creator
-
-        description = self.request.form.get('description')
-        self.context.description = description
-
-        identifier = self.request.form.get('identifier')
-        self.context.identifier = identifier
-
-        publisher = self.request.form.get('publisher')
-        self.context.publisher = publisher
-
-        relation = self.request.form.get('relation')
-        self.context.relation = relation
-
-        rights = self.request.form.get('rights')
-        self.context.rights = rights
-
-        source = self.request.form.get('source')
-        self.context.source = source
-
-        dublin_core_type = self.request.form.get('dublin_core_type')  # type
-        self.context.dublin_core_type = dublin_core_type  # is a reserved word
-
-        self.context.reindexObject()
-
-        return self.request.response.redirect(self.context.absolute_url())
 
 
-class ImageArtifactEditTags():
-    """
-    Browser page
-    """
 
-    def __call__(self):
-        """
-        Set tags; redirect to context
-        """
-        tags = self.request.form.get('tags').split('\r\n')
-        self.context.setSubject(tags)
-        self.context.reindexObject()
-        return self.request.response.redirect(self.context.absolute_url())
+    # XXX Poor man's Dublin Core (re)implementation
+    coverage = TextLine(title=u"Coverage", required=False)
 
+    dublin_core_format = TextLine(title=u"Format", required=False)
 
-class ImageArtifactView():
-    """
-    Browser page
-    """
+    identifier = TextLine(title=u"Identifier", required=False)
 
-    image_artifact_view = ViewPageTemplateFile('image_artifact.pt')
+    language = TextLine(title=u"Language", required=False)
 
-    def __call__(self):
-        """
-        Return the view template; handle form posts
-        """
-        add_another_question = False
-        if self.request.method == 'POST':
-            questions = []
-            items = self.request.form.items()  # XXX Why not use .get()?
-            for item, text in items:  # Add another question?
-                if item != 'new-question':  # No
-                    questions.append((item, text))
-                else:  # Yes
-                    add_another_question = True
-            annotation = createContentInContainer(self.context, 'annotation')
-            for uid, text in questions:  # Process questions
-                if text is not '':
-                    question = uuidToObject(uid)
-                    question = question.Title()
-                    title = "Response to %s"
-                    response = createContentInContainer(
-                        annotation, 'response', title=title % question)
-                    storage = IStorage(response)
-                    storage[uid] = question
-                    response.description = text
-            if add_another_question:
-                portal = self.context.portal_url()
-                self.request.response.redirect("%s/++add++question" % portal)
-        return self.image_artifact_view()
+    publisher = TextLine(title=u"Publisher", required=False)
 
-    def get_questions(self):
-        """
-        Return content items of type "question" from the catalog
-        """
-        return self.context.portal_catalog(
-            portal_type="question", sort_on="id", sort_order="ascending")
+    relation = TextLine(title=u"Relation", required=False)
 
-    def get_contributor(self):
-        """
-        Return contributor field as a string separated by newlines
-        """
-        return '\n'.join(self.context.Contributors())
+    rights = TextLine(title=u"Rights", required=False)
 
-    def get_creator(self):
-        """
-        Return creator field as a string separated by newlines
-        """
-        return '\n'.join(self.context.creators)
+    source = TextLine(title=u"Source", required=False)
 
-    def get_tags(self):
-        """
-        Return tags as a string separated by newlines
-        """
-        return '\n'.join(self.context.Subject())
+    subject = TextLine(title=u"Subject", required=False)
+
+    dublin_core_type = TextLine(title=u"Type", required=False)
