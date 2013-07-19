9755b6d2324518b09bc5478e2950d079177b94eb
Alex Clark <aclark@aclark.net>
2013-07-19 13:41:37 -0400

    Clean up

--- rpi/asthma_files_site/content/image_artifact.py
+++ rpi/asthma_files_site/content/image_artifact.py
@@ -2,18 +2,6 @@ from plone.directives import form
 from plone.namedfile.field import NamedBlobImage
 from zope.schema import TextLine
 
-# XXX Poor man's Dublin Core (re)implementation
-#from .dublin_core import _coverage
-#from .dublin_core import _format
-#from .dublin_core import _identifier
-#from .dublin_core import _language
-#from .dublin_core import _publisher
-#from .dublin_core import _relation
-#from .dublin_core import _rights
-#from .dublin_core import _source
-#from .dublin_core import _subject
-#from .dublin_core import _type
-
 
 class IImageArtifact(form.Schema):
     """
@@ -22,23 +10,6 @@ class IImageArtifact(form.Schema):
 
     image_artifact = NamedBlobImage(title=u"Image Artifact")
 
-    # XXX Poor man's Dublin Core (re)implementation
-#    coverage = _coverage
-#    dublin_core_format = _format
-#    identifier = _identifier
-#    language = _language
-#    publisher = _publisher
-#    relation = _relation
-#    rights = _rights
-#    source = _source
-#    subject = _subject
-#    dublin_core_type = _type
-
-
-
-
-
-    # XXX Poor man's Dublin Core (re)implementation
     coverage = TextLine(title=u"Coverage", required=False)
 
     dublin_core_format = TextLine(title=u"Format", required=False)
