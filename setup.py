9c85dd30c609594de9b9a32838c39e420c7b94a3
Alex Clark <aclark@aclark.net>
2013-07-23 16:35:52 -0400

    Add file(s)

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     },
     install_requires=[
         'setuptools',
+        'collective.documentviewer',
         'five.grok',
         'plone.app.referenceablebehavior',
         'pyzotero',
