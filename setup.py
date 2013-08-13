f1ac5b74043929480de33ed442d668dcbfb04af6
Alex Clark <aclark@aclark.net>
2013-08-13 01:37:31 -0400

    Add meta

--- setup.py
+++ setup.py
@@ -5,11 +5,23 @@ from setuptools import setup
 setup(
     author="Alex Clark",
     author_email="aclark@aclark.net",
+    classifiers=[
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python :: 2.7",
+    ],
     description="Asthma Files Site",
+    keywords=[
+        "Plone",
+    ],
+    license="GPL",
     long_description=open("README.rst").read(),
     name='rpi.asthma_files_site',
     packages=find_packages(),
-    namespace_packages=['rpi'],
+    test_suite="rpi.asthma_files_site.tests",
+    url="https://github.com/ACLARKNET/rpi.asthma_files_site",
+    namespace_packages=[
+        'rpi'
+    ],
     entry_points={
         'z3c.autoinclude.plugin': 'target = plone',
     },
@@ -20,4 +32,6 @@ setup(
         'pyzotero',
         'z3c.jbot',
     ],
+    version="0.0.1",
+    zip_safe="True",
 )
