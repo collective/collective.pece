10322f46fafbe18a4eff229a154ba94c01a5cd0c
Alex Clark <aclark@aclark.net>
2013-09-08 18:07:48 -0400

    Add test

--- rpi/asthma_files_site/tests.py
+++ rpi/asthma_files_site/tests.py
@@ -1,9 +1,35 @@
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
 import unittest
 
 
-class AsthmaFilesSiteTests(unittest.TestCase):
+class Fixture(PloneSandboxLayer):
     """
     """
+
+    def setUpPloneSite(self, portal):
+        self.applyProfile(portal, 'rpi.asthma_files_site:default')
+
+
+FIXTURE = Fixture()
+
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,),
+    name='rpi.asthma_files_site:Integration',
+    )
+
+
+class TestAsthmaFilesSite(unittest.TestCase):
+    """
+    """
+
+    layer = INTEGRATION_TESTING
+
+    def test_add_artifact_image(self):
+        """
+        """
+        import pdb ; pdb.set_trace()
 
 
 if __name__ == '__main__':
