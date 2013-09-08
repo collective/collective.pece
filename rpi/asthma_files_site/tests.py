4aedd0a376308bdaa9c7f101d5cb767e4fa9f36e
Alex Clark <aclark@aclark.net>
2013-09-08 18:57:12 -0400

    Add test(s)

--- rpi/asthma_files_site/tests.py
+++ rpi/asthma_files_site/tests.py
@@ -11,6 +11,13 @@ class TestAsthmaFilesSiteContentTypes(PloneTestCase.PloneTestCase):
     Test Asthma Files Site content types
     """
 
+    def test_add_audio_artifact(self):
+        """
+        Test add audio artifact to folder
+        """
+        self.folder.invokeFactory('audio_artifact', 'asthma.ogg')
+        assert 'asthma.ogg' in self.folder
+
     def test_add_document_artifact(self):
         """
         Test add document artifact to folder
@@ -24,6 +31,13 @@ class TestAsthmaFilesSiteContentTypes(PloneTestCase.PloneTestCase):
         """
         self.folder.invokeFactory('image_artifact', 'asthma.jpg')
         assert 'asthma.jpg' in self.folder
+
+    def test_add_video_artifact(self):
+        """
+        Test add video artifact to folder
+        """
+        self.folder.invokeFactory('video_artifact', 'asthma.ogv')
+        assert 'asthma.ogv' in self.folder
 
 
 if __name__ == '__main__':
