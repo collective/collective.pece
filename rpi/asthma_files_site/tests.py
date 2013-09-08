0868c99a3191e4aef82167da1bd29781af3eb619
Alex Clark <aclark@aclark.net>
2013-09-08 18:54:48 -0400

    Wording [ci skip]

--- rpi/asthma_files_site/tests.py
+++ rpi/asthma_files_site/tests.py
@@ -13,14 +13,14 @@ class TestAsthmaFilesSiteContentTypes(PloneTestCase.PloneTestCase):
 
     def test_add_document_artifact(self):
         """
-        Test adding document artifact to a folder
+        Test add document artifact to folder
         """
         self.folder.invokeFactory('document_artifact', 'asthma.pdf')
         assert 'asthma.pdf' in self.folder
 
     def test_add_image_artifact(self):
         """
-        Test adding an image artifact to a folder
+        Test add image artifact to folder
         """
         self.folder.invokeFactory('image_artifact', 'asthma.jpg')
         assert 'asthma.jpg' in self.folder
