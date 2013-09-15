fe77f2b255752c6fef17306a74135e92e4475584
Alex Clark <aclark@aclark.net>
2013-09-15 08:51:56 -0400

    Add comment

--- collective/pece/content/audio_artifact.py
+++ collective/pece/content/audio_artifact.py
@@ -5,7 +5,10 @@ from zope.schema import TextLine
 
 class IAudioArtifact(form.Schema):
     """
-    Content type
+    The PECE artifact content types attempt to provide easier access to Dublin
+    Core Metadata (i.e. easier than Plone core's implementation):
+
+    http://dublincore.org/documents/dces/
     """
 
     # XXX How else can I make title field optional (i.e. without turning
@@ -18,21 +21,12 @@ class IAudioArtifact(form.Schema):
     form.primary('audio_artifact')
 
     coverage = TextLine(title=u"Coverage", required=False)
-
     format_ = TextLine(title=u"Format", required=False)
-
     identifier = TextLine(title=u"Identifier", required=False)
-
     language = TextLine(title=u"Language", required=False)
-
     publisher = TextLine(title=u"Publisher", required=False)
-
     relation = TextLine(title=u"Relation", required=False)
-
     rights = TextLine(title=u"Rights", required=False)
-
     source = TextLine(title=u"Source", required=False)
-
     subject = TextLine(title=u"Subject", required=False)
-
     type_ = TextLine(title=u"Type", required=False)
